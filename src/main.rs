f69dae18713378744d1e33dc6f24096e8d41b79a
Romain Chardiny <romain.chardiny@gmail.com>
2022-06-23 10:37:52 +0200

    move ask player

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,22 @@ mod ai;
 mod game;
 mod utils;
 
+fn ask_player() -> Option<usize> {
+    print!("Column: ");
+
+    let mut input = String::new();
+
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    match input.trim().parse::<usize>() {
+        Ok(num) => Some(num),
+        Err(_) => None,
+    }
+}
+
 fn main() {
     let mut board = game::Board::new();
     let best_ai = ai::minimax::MinimaxAI::new(&board);
@@ -12,21 +28,8 @@ fn main() {
     loop {
         let column: Option<usize> = match player_turn {
             true => {
-                print!("Column: ");
-
-                let mut input = String::new();
-
-                io::stdout().flush().unwrap();
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read line");
-
                 player_turn = false;
-
-                match input.trim().parse::<usize>() {
-                    Ok(num) => Some(num),
-                    Err(_) => None,
-                }
+                ask_player()
             }
             false => {
                 player_turn = true;
