85e83572b02b9cbf32b6a3db424ada8f03add3df
Romain Chardiny <romain.chardiny@gmail.com>
2022-06-23 12:25:26 +0200

    dumb thing

--- src/ai/random.rs
+++ src/ai/random.rs
@@ -3,6 +3,12 @@ use rand::Rng;
 
 pub struct RandomAI {}
 
+impl RandomAI {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
 impl ai::AI for RandomAI {
     fn choose_column(&self) -> usize {
         rand::thread_rng().gen_range(0..6)
